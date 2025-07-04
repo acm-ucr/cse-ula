56ad536825f5cd70efba878c00e7f63a35540c97
Simon <simoncun2586@gmail.com>
2025-07-04 15:18:29 -0700

    OurGoals Component that takes in props

--- src/components/about/OurGoals.tsx
+++ src/components/about/OurGoals.tsx
@@ -1,4 +1,16 @@
-const OurGoals = () => {
-  return <div className="">OurGoals</div>;
+import { StaticImageData } from "next/image";
+import Image from "next/image"
+
+type OurGoalsProps = {
+  image : StaticImageData;
+  text: string;
+}
+const OurGoals = ({image, text}: OurGoalsProps) => {
+  return (
+  <div className="flex flex-col items-center justify-center border-y-4 mx-20 py-12 border-ula-yellow-primary font-medium">
+    <Image src={image} alt="logo"/>
+    {text}
+  </div>
+  );
 };
 export default OurGoals;
