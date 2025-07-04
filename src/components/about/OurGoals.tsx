b975e6dfa15ff82abbaa72a31ba861956ce5d224
Simon <simoncun2586@gmail.com>
2025-07-04 15:29:14 -0700

    separated the components into another props comonent and called the props component in Our Goals

--- src/components/about/OurGoals.tsx
+++ src/components/about/OurGoals.tsx
@@ -1,16 +1,8 @@
-import { StaticImageData } from "next/image";
-import Image from "next/image";
-
-type OurGoalsProps = {
-  image: StaticImageData;
-  text: string;
-};
-const OurGoals = ({ image, text }: OurGoalsProps) => {
+import OurGoalsProps from "@/components/about/OurGoalsProps";
+import myImage from "@/public/image.jpg"
+const OurGoals = () => {
   return (
-    <div className="mx-20 flex flex-col items-center justify-center border-y-4 border-ula-yellow-primary py-12 font-medium">
-      <Image src={image} alt="logo" />
-      {text}
-    </div>
+    <OurGoalsProps image={myImage} text="placeholder"/>
   );
 };
 export default OurGoals;
---
+++ src/components/about/OurGoalsProps.tsx
@@ -0,0 +1,16 @@
+import { StaticImageData } from "next/image";
+import Image from "next/image";
+
+type OurGoalsProps = {
+  image: StaticImageData;
+  text: string;
+};
+const OurGoalsProps = ({ image, text }: OurGoalsProps) => {
+  return (
+    <div className="mx-20 flex flex-col items-center justify-center border-y-4 border-ula-yellow-primary py-12 font-medium">
+      <Image src={image} alt="logo" />
+      {text}
+    </div>
+  );
+};
+export default OurGoalsProps;
