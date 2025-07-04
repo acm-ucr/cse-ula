8ed5aff7fd3afbe7cd05d62d42a1eab54370299c
Simon <simoncun2586@gmail.com>
2025-07-04 15:32:51 -0700

    formatting

--- src/components/about/OurGoals.tsx
+++ src/components/about/OurGoals.tsx
@@ -1,6 +1,11 @@
 import OurGoalsProps from "@/components/about/OurGoalsProps";
 import Book from "@/public/about/book.svg";
 const OurGoals = () => {
-  return <OurGoalsProps image={Book} text="Improve the learning experience of students in core CS courses" />;
+  return (
+    <OurGoalsProps
+      image={Book}
+      text="Improve the learning experience of students in core CS courses"
+    />
+  );
 };
 export default OurGoals;
