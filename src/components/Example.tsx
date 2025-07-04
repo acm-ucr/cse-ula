6505ae17df825bbc0d929cd8171f8179ed212256
lydia son <lymifrog@gmail.com>
2025-07-03 19:57:28 -0700

    Flexbox Practice

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,18 +1,20 @@
-const Example = (props: { text1: string; text2: string; text3: string }) => {
+const Example = (props: { text1?: string; text2: string; text3: string }) => {
   return (
-    <div className="flex w-3/4 items-center gap-1 bg-red-200 p-10">
-      <div className="w-1/2 bg-ula-blue-primary p-5 text-left">
-        {props.text1}
+    <div className="flex w-2/3 flex-col items-center gap-y-12 rounded-md bg-red-200 p-10 text-center">
+      <div className="w-1/2 bg-ula-blue-primary p-5 text-center text-white">
+        {"Some"}
       </div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="w-full bg-ula-blue-accent p-5 text-left">
-          props.text2
+      <div className="flex w-1/2 flex-row gap-x-1.5">
+        <div className="px-15 w-full rounded-full bg-ula-yellow-primary py-5 text-center">
+          {props.text3}
         </div>
-        <div className="w-full bg-ula-yellow-accent p-5 text-right">
-          {props.text2}
+        <div className="px-15 w-full rounded-full bg-ula-yellow-accent py-5 text-center">
+          {props.text3}
         </div>
       </div>
-      {/* Hint: Add another box/div here similarly to line 4 */}
+      <div className="flex w-1/2 flex-col bg-ula-blue-accent p-5 text-center">
+        {"Here"}
+      </div>
     </div>
   );
 };
