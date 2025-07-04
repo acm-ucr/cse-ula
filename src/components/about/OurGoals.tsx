303be34f0909de19640c88ae65ebfae669029121
Simon <simoncun2586@gmail.com>
2025-07-04 15:18:52 -0700

    formatting

--- src/components/about/OurGoals.tsx
+++ src/components/about/OurGoals.tsx
@@ -1,16 +1,16 @@
 import { StaticImageData } from "next/image";
-import Image from "next/image"
+import Image from "next/image";
 
 type OurGoalsProps = {
-  image : StaticImageData;
+  image: StaticImageData;
   text: string;
-}
-const OurGoals = ({image, text}: OurGoalsProps) => {
+};
+const OurGoals = ({ image, text }: OurGoalsProps) => {
   return (
-  <div className="flex flex-col items-center justify-center border-y-4 mx-20 py-12 border-ula-yellow-primary font-medium">
-    <Image src={image} alt="logo"/>
-    {text}
-  </div>
+    <div className="mx-20 flex flex-col items-center justify-center border-y-4 border-ula-yellow-primary py-12 font-medium">
+      <Image src={image} alt="logo" />
+      {text}
+    </div>
   );
 };
 export default OurGoals;
