38a6d9897a21df031aec754e87659c2d5f1b0b5b
Kevin Loritsch <kloritsch58@gmail.com>
2025-07-04 14:43:07 -0700

    small tweak

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import RiversideLogo from "@/public/riversideLogo.webp";
+import riversideLogo from "@/public/riversideLogo.webp";
 import { FaDiscord } from "react-icons/fa";
 import Link from "next/link";
 const Footer = () => {
   return (
-    <div className="flex items-center justify-between bg-ula-blue-primary p-10 text-white">
+    <div className="flex items-center justify-between bg-ula-blue-primary p-8 text-white">
       <div className="flex flex-col items-center justify-center">
-        <Image src={RiversideLogo} alt="Logo of UCR" />
+        <Image src={riversideLogo} alt="Logo of UCR" />
         <div className="w-3/4 text-center text-xl">
           Undergraduate Learning Assistants
         </div>
