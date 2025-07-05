35306026fee7d5dbc466b2b2432f46de914d5d3a
Simon <simoncun2586@gmail.com>
2025-07-05 15:03:39 -0700

    merged the files I made unecessarily

--- src/components/about/OurGoals.tsx
+++ src/components/about/OurGoals.tsx
@@ -1,5 +1,21 @@
-import OurGoalsProps from "@/components/about/OurGoalsProps";
 import Book from "@/public/about/book.svg";
+import { StaticImageData } from "next/image";
+import Image from "next/image";
+
+type OurGoalsProps = {
+  image: StaticImageData;
+  text: string;
+};
+
+const OurGoalsProps = ({ image, text }: OurGoalsProps) => {
+  return (
+    <div className="mx-24 flex flex-col items-center justify-center gap-y-4 border-y-[5px] border-ula-yellow-primary py-10 text-center text-xl font-medium">
+      <Image src={image} alt="logo" />
+      <div className="px-10">{text}</div>
+    </div>
+  );
+};
+
 const OurGoals = () => {
   return (
     <OurGoalsProps
