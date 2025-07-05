c48a8e595a9886b1d62e375815bc8cb4f1131d23
ALE00111 <austintle56@gmail.com>
2025-07-04 17:39:21 -0700

    Update

--- src/components/home/InfoBanner.tsx
+++ src/components/home/InfoBanner.tsx
@@ -1,11 +1,9 @@
 import Location from "@/components/home/Location";
-import Description from "@/components/home/Description";
 
 const InfoBanner = () => {
   return (
     <div className="">
       InfoBanner
-      <Description />
       <Location />
     </div>
   );
