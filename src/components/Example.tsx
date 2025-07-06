f6aaa945c1c9dde0274247091ce95003495ce487
SM Kamal <smkamal1005@gmail.com>
2025-07-06 00:32:59 -0700

    Formatting fixes done

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,12 +1,16 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="flex h-1/2 w-7/12 flex-col  items-center justify-center gap-12 rounded-lg bg-red-200">
-      <div className="w-1/2 bg-ula-blue-primary p-5 text-center text-white mb-5">
+    <div className="flex h-1/2 w-7/12 flex-col items-center justify-center gap-12 rounded-lg bg-red-200">
+      <div className="mb-5 w-1/2 bg-ula-blue-primary p-5 text-center text-white">
         {props.text1}
       </div>
       <div className="flex w-1/2 flex-row gap-1 text-center">
-        <div className="w-full bg-ula-yellow-primary p-5 rounded-full mb-5">{props.text3}</div>
-        <div className="w-full bg-ula-yellow-accent p-5 rounded-full mb-5">{props.text3}</div>
+        <div className="mb-5 w-full rounded-full bg-ula-yellow-primary p-5">
+          {props.text3}
+        </div>
+        <div className="mb-5 w-full rounded-full bg-ula-yellow-accent p-5">
+          {props.text3}
+        </div>
       </div>
       <div className="w-1/2 bg-ula-blue-accent p-5 text-center">
         {props.text2}
