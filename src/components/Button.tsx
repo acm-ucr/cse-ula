519ae6bb43ddefa8104b1e3e4b07548ee8a44255
pratheek28 <pratheek0928@gmail.com>
2025-07-12 09:20:31 -0700

    Fixed spacing to use tailwind commands

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,8 +4,8 @@ import buttonArrow from "@/public/buttonArrow.svg";
 const Button = (props: { text1: string }) => {
   return (
     <div className="rounded-2xl bg-ula-yellow-accent p-3 font-semibold">
-      <button className="flex flex-row pl-2">
-        {props.text1}&emsp;&emsp;&emsp;{" "}
+      <button className="flex flex-row gap-14 pl-2">
+        {props.text1}
         <Image src={buttonArrow} alt={"Arrow"} />{" "}
       </button>
     </div>
