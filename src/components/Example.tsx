32178b989b21df4c0409ea4b93e761ff83e12526
gutlapallin <nehagutlapalli@gmail.com>
2025-07-11 17:37:14 -0700

    Flexbox Pratice Exercise

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,17 +1,23 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="flex w-3/4 items-center gap-1 bg-red-200 p-10">
-      <div className="w-1/2 bg-ula-blue-primary p-5 text-left">
-        {props.text1}
-      </div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="w-full bg-ula-blue-accent p-5 text-left">
-          props.text2
+    <div className="flex w-2/3 items-center gap-10 bg-red-200 p-10">
+      <div className="flex w-full flex-col gap-10">
+        <div className="w-full rounded-sm bg-ula-blue-primary p-5 text-center text-white">
+          {props.text1}
         </div>
-        <div className="w-full bg-ula-yellow-accent p-5 text-right">
-          {props.text2}
+        <div className="flex w-full flex-row gap-1">
+          <div className="w-1/2 rounded-full bg-ula-yellow-primary p-5 text-center">
+            {props.text2}
+          </div>
+          <div className="inline-block w-1/2 rounded-full bg-ula-yellow-accent p-5 text-center">
+            {props.text2}
+          </div>
+        </div>
+        <div className="w-full rounded-sm bg-ula-blue-accent p-5 text-center">
+          {props.text3}
         </div>
       </div>
+
       {/* Hint: Add another box/div here similarly to line 4 */}
     </div>
   );
