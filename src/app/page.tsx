2c8718b5442ce49d7fe13f816c7689cb463efc8e
Victoria Zambrano <victoriaz20033@gmail.com>
2025-07-12 20:45:48 -0700

    Implement Home Page Map and Description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Example from "@/components/Example";
+// import Example from "@/components/Example";
+import Location from "@/components/home/Location";
 
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center text-center text-2xl">
-      <Example text1="Adjust" text2="These" text3="Props" />
+      <Location mapText="Find us at Room 129 on WCH Floor 1!" />
     </div>
   );
 };
 
 export default Home;
+//<Example text1="Adjust" text2="These" text3="Props" />
--- src/components/home/Location.tsx
+++ src/components/home/Location.tsx
@@ -1,4 +1,16 @@
-const Location = () => {
-  return <div className="">Location</div>;
+import Image from "next/image";
+import ulaMap from "@/public/home/ulaMap.svg";
+
+const Location = (props: { mapText: string }) => {
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-1 bg-ula-blue-primary p-8">
+      <>
+        <Image src={ulaMap} alt={"ulaMap"} />
+      </>
+      <div className="flex items-center justify-center bg-transparent text-center text-white p-f">
+        {props.mapText}
+      </div>
+    </div>
+  );
 };
 export default Location;
