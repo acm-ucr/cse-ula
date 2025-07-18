0bbaf57dcb4df9b2d9641df7689a1a3fdc3aab84
LivayaaSharma <livayaasharma777@gmail.com>
2025-07-18 16:24:31 +0530

    added optimized professor quote with image and made requested changes in the page.tsx format

--- src/components/about/Professor.tsx
+++ src/components/about/Professor.tsx
@@ -1,21 +1,27 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const Professor = () => {
   return (
-    <div className="flex flex-col bg-white py-10 px-4 items-center   ">
-      <Image src="/about/watkinsonMedina.webp" alt="Professor Watkinson Medina"          
+    <div className="flex flex-col items-center bg-white px-4 py-10">
+      <Image
+        src="/about/watkinsonMedina.webp"
+        alt="Professor Watkinson Medina"
         width={200}
         height={200}
-        className="mb-6 rounded-sm object-cover" />
-     <div className="max-w-2xl text-center">
-      <p className="text-lg font-medium text-black leading-relaxed">
-        "The ULA program is a great chance to make a real difference.
-        Students get more personalized learning that helps with their classes, and ULAs get to grow as educators. All in all, the ULA program is a good thing for everyone who's part of it."
-      </p>
-      <p className="mt-2 text-right font-medium">-Professor Watkinson Medina</p>
-    </div>
+        className="mb-6 rounded-sm object-cover"
+      />
+      <div className="max-w-2xl text-center">
+        <p className="text-lg font-medium leading-relaxed text-black">
+          "The ULA program is a great chance to make a real difference. Students
+          get more personalized learning that helps with their classes, and ULAs
+          get to grow as educators. All in all, the ULA program is a good thing
+          for everyone who's part of it."
+        </p>
+        <p className="mt-2 text-right font-medium">
+          -Professor Watkinson Medina
+        </p>
+      </div>
     </div>
   );
 };
 export default Professor;
-
