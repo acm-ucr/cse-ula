92a282bfb77ee27e5bfbfd9414aafbbf24322ad9
Kevin Loritsch <kloritsch58@gmail.com>
2025-07-19 14:00:24 -0700

    capitalization

--- src/components/about/CourseOfferingCard.tsx
+++ src/components/about/CourseOfferingCard.tsx
@@ -1,8 +1,8 @@
-type CourseOfferingCardProps = {
+type courseOfferingCardProps = {
   course: string;
 };
 
-const CourseOfferingCard = ({ course }: CourseOfferingCardProps) => {
+const CourseOfferingCard = ({ course }: courseOfferingCardProps) => {
   return (
     <div className="flex aspect-[11/12] w-full items-center justify-center rounded-xl bg-ula-blue-accent text-center text-3xl font-semibold">
       {course}
