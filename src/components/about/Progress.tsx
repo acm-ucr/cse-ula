6768371074d2f3068e6db06585d613cc96551f96
Kevin Loritsch <kloritsch58@gmail.com>
2025-07-19 15:18:08 -0700

    tweaks

--- src/components/about/Progress.tsx
+++ src/components/about/Progress.tsx
@@ -1,29 +1,29 @@
 "use client";
+
 import CountUp from "react-countup";
+
 const Progress = () => {
   return (
     <div className="flex w-full flex-col items-center justify-center bg-ula-blue-primary">
-      <div className="flex justify-center p-4 text-center md:p-8">
-        <div className="w-full text-center font-bold text-white sm:text-4xl">
-          The UCR CS ULA program started in Fall 2021. Since then we have…
-        </div>
-      </div>
-      <div className="mb-12 flex w-1/2 flex-col items-center justify-center sm:flex-row">
+      <p className="flex w-full justify-center p-8 text-center font-bold text-white sm:text-4xl">
+        The UCR CS ULA program started in Fall 2021. Since then we have…
+      </p>
+      <div className="mb-8 flex w-1/2 flex-row items-center justify-center">
         <div className="flex w-1/2 flex-col items-center justify-center">
-          <div className="text-center text-5xl font-extrabold text-ula-yellow-primary md:text-7xl">
-            <CountUp start={0} duration={2} end={1500} separator="" />+
+          <div className="text-center text-6xl font-extrabold text-ula-yellow-primary">
+            <CountUp start={0} duration={2} end={1500} />+
           </div>
-          <div className="text-nowrap p-2 text-center text-white md:text-xl">
+          <p className="text-nowrap p-2 text-center text-white">
             students helped
-          </div>
+          </p>
         </div>
         <div className="flex w-1/2 flex-col items-center justify-center">
-          <div className="text-center text-5xl font-extrabold text-ula-yellow-primary md:text-7xl">
-            <CountUp start={0} end={30} duration={2} separator="" />+
+          <div className="text-center text-6xl font-extrabold text-ula-yellow-primary">
+            <CountUp start={0} end={30} duration={2.5} />+
           </div>
-          <div className="text-nowrap p-2 text-center text-white md:text-xl">
+          <p className="text-nowrap p-2 text-center text-white">
             tutors trained
-          </div>
+          </p>
         </div>
       </div>
     </div>
