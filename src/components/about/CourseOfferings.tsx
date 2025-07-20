d38c3835a613a5e31221b46d827ac831d57b497c
Simon <simoncun2586@gmail.com>
2025-07-20 12:34:52 -0700

    initial commit

--- src/components/about/CourseOfferings.tsx
+++ src/components/about/CourseOfferings.tsx
@@ -1,15 +1,43 @@
 import CourseOfferingCard from "@/components/about/CourseOfferingCard";
+import { Courses } from "@/data/CourseOfferings";
+
 const CourseOfferings = () => {
+  const firstRow = Courses.slice(0, 4);
+  const secondRow = Courses.slice(4, 7);
   return (
-    <div className="grid grid-cols-4 gap-10 px-64">
-      <CourseOfferingCard course="9 series" />
-      <CourseOfferingCard course="9 series" />
-      <CourseOfferingCard course="9 series" />
-      <CourseOfferingCard course="9 series" />
-      <CourseOfferingCard course="9 series" />
-      <CourseOfferingCard course="9 series" />
-      <CourseOfferingCard course="9 series" />
+    <div className="py-20">
+      <div className="flex w-full flex-col items-center justify-center">
+        <div className="text-7xl font-bold text-ula-blue-primary">
+          Course Offerings
+        </div>
+        <div className="mx-64 py-10 text-center text-3xl leading-loose">
+          Our ULAs are here to support you through office hours, and directly in
+          lab sections. Starting Week 2 of each quarter, each class listed below
+          will have one or more ULAs there to offer you direct assistance!
+        </div>
+      </div>
+      <div className="mb-10 flex justify-center gap-x-10 px-64">
+        {firstRow.map((course, index) => (
+          <div key={index} className="flex w-[22%] justify-center">
+            <CourseOfferingCard
+              course={course.course}
+              color={index % 2 === 0 ? "blue" : "yellow"}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center gap-x-10 px-64">
+        {secondRow.map((course, index) => (
+          <div key={index} className="flex w-[22%] justify-center">
+            <CourseOfferingCard
+              course={course.course}
+              color={(index + 4) % 2 === 0 ? "blue" : "yellow"}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
+
 export default CourseOfferings;
--- src/data/CourseOfferings.ts
+++ src/data/CourseOfferings.ts
@@ -1,2 +1,23 @@
-//Put Name of course
-//Delete Comments
+export const Courses = [
+  {
+    course: "9 series",
+  },
+  {
+    course: "10 series",
+  },
+  {
+    course: "CS011",
+  },
+  {
+    course: "CS111",
+  },
+  {
+    course: "CS141",
+  },
+  {
+    course: "CS061",
+  },
+  {
+    course: "CS100",
+  },
+];
