0b4262c71138b39dd404e99cafa3c754cb0229c3
Willow <isaiasnoel@gmail.com>
2025-07-20 21:34:26 -0700

    flip implemented, card mostly complete now

--- src/components/ula/Card.tsx
+++ src/components/ula/Card.tsx
@@ -1,23 +1,50 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
+import { motion } from "motion/react";
 import Angel from "@/public/ulas/angel.webp";
 
 const Card = () => {
+  const [flipped, setFlipped] = useState(false);
+
   return (
-    <div className="flex w-1/5 flex-col overflow-hidden rounded-xl">
-      {/* image portion */}
-      <div className="aspect-[4/3] bg-gray-300">
-        <Image
-          src={Angel}
-          alt="Image of Angel"
-          className="h-full w-full object-cover"
-        />
+    <motion.div
+      whileHover={{ scale: 1.05 }}
+      transition={{ duration: 0.5 }}
+      animate={{ rotateY: flipped ? 180 : 0 }}
+      onClick={() => setFlipped((prevState) => !prevState)}
+      className="relative m-4 flex cursor-pointer flex-col items-center"
+      style={{ transformStyle: "preserve-3d" }}
+    >
+      {/* front side */}
+      <div
+        className="flex w-1/5 flex-col overflow-hidden rounded-xl"
+        style={{ backfaceVisibility: "hidden", transform: "rotateY(0deg)" }}
+      >
+        {/* image portion */}
+        <div className="aspect-[4/3]">
+          <Image
+            src={Angel}
+            alt="Image of Angel"
+            className="h-full w-full object-cover"
+          />
+        </div>
+        {/* bottom text portion */}
+        <div className="h-1/8 flex flex-col items-center justify-center bg-gradient-to-t from-ula-yellow-primary to-ula-yellow-accent py-3 text-2xl font-bold">
+          <div>{"Angel"}</div>
+          <div>{"CS10ABC"}</div>
+        </div>
       </div>
-      {/* text portion */}
-      <div className="h-1/8 flex flex-col items-center justify-center bg-gradient-to-t from-ula-yellow-primary to-ula-yellow-accent py-3 text-2xl font-bold">
-        <div>{"Angel"}</div>
-        <div>{"CS10ABC"}</div>
+      {/* backs side */}
+      <div
+        className="absolute flex w-1/5 items-center justify-center rounded-xl border-8 border-ula-yellow-accent text-center text-base"
+        style={{ backfaceVisibility: "hidden", transform: "rotateY(180deg)" }}
+      >
+        {
+          "Hi! My name is Angel Franco and I am a 3rd year Math major with a concentration in Computational Math. I became a ULA because I like to help. If you ever need help with CS10ABC I got you! If you like One Piece we can talk about that too! (Just don't spoil because I don't read the Manga)"
+        }
       </div>
-    </div>
+    </motion.div>
   );
 };
 
