33c8496a234c7e0903a4b58161a13013eedb0639
Willow <isaiasnoel@gmail.com>
2025-07-23 11:28:24 -0700

    implemented requested changes

--- src/components/ula/Card.tsx
+++ src/components/ula/Card.tsx
@@ -1,10 +1,16 @@
 "use client";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import { useState } from "react";
 import { motion } from "motion/react";
-import Angel from "@/public/ulas/angel.webp";
 
-const Card = () => {
+interface ComponentProps {
+  image: StaticImageData;
+  name: string;
+  classes: string;
+  description: string;
+}
+
+const Card = ({ image, name, classes, description }: ComponentProps) => {
   const [flipped, setFlipped] = useState(false);
 
   return (
@@ -13,32 +19,30 @@ const Card = () => {
       transition={{ duration: 0.5 }}
       animate={{ rotateY: flipped ? 180 : 0 }}
       onClick={() => setFlipped((prevState) => !prevState)}
-      className="relative m-4 flex cursor-pointer flex-col items-center"
+      className="relative m-4 flex w-1/5 cursor-pointer flex-col items-center"
       style={{ transformStyle: "preserve-3d" }}
     >
       <div
-        className="flex w-1/5 flex-col overflow-hidden rounded-xl"
+        className="flex flex-col overflow-hidden rounded-xl"
         style={{ backfaceVisibility: "hidden", transform: "rotateY(0deg)" }}
       >
         <div className="aspect-[4/3]">
           <Image
-            src={Angel}
-            alt="Image of Angel"
+            src={image}
+            alt="Angel"
             className="h-full w-full object-cover"
           />
         </div>
-        <div className="h-1/8 flex flex-col items-center justify-center bg-ula-yellow-accent py-3 text-2xl font-bold">
-          <div>{"Angel"}</div>
-          <div>{"CS10ABC"}</div>
+        <div className="h-1/8 flex flex-col items-center justify-center bg-ula-yellow-accent py-3 text-2xl font-bold shadow-[0_-20px_40px_rgba(255,200,0,.8)]">
+          <p>{name}</p>
+          <p>{classes}</p>
         </div>
       </div>
       <div
-        className="absolute flex w-1/5 items-center justify-center rounded-xl border-8 border-ula-yellow-accent text-center text-base"
+        className="absolute flex h-full items-center justify-center rounded-xl border-8 border-ula-yellow-accent text-center text-base"
         style={{ backfaceVisibility: "hidden", transform: "rotateY(180deg)" }}
       >
-        {
-          "Hi! My name is Angel Franco and I am a 3rd year Math major with a concentration in Computational Math. I became a ULA because I like to help. If you ever need help with CS10ABC I got you! If you like One Piece we can talk about that too! (Just don't spoil because I don't read the Manga)"
-        }
+        {description}
       </div>
     </motion.div>
   );
