5c8c658dfae7d275c9170c86e3853fc2f29b66fe
Victoria Zambrano <victoriaz20033@gmail.com>
2025-07-27 11:18:36 -0500

    [Incomplete] Mobilizing Our Goals Section

--- src/components/about/OurGoals.tsx
+++ src/components/about/OurGoals.tsx
@@ -6,7 +6,7 @@ const OurGoals = () => {
   return (
     <div className="py-8">
       <Header text="Our Goals" />
-      <div className="flex w-full justify-evenly pt-6">
+      <div className="flex w-full flex-wrap justify-evenly pt-6 md:flex-nowrap">
         {ourGoalsCard.map(({ image, text }, index) => (
           <OurGoalsCard key={index} image={image} text={text} />
         ))}
