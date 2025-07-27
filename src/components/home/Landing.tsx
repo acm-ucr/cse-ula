afad3f1f59e9a641faae58f5475a9793607ddddc
SM Kamal <smkamal1005@gmail.com>
2025-07-27 14:03:30 -0700

    Comment Fixes

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -1,21 +1,18 @@
 import Image from "next/image";
-import LandingPage from "@/public/home/WCHLanding.webp";
+import landingPage from "@/public/home/WCHLanding.webp";
 
 const Landing = () => {
   return (
-    <div className="relative h-[960px] w-full">
+    <div className="relative h-[800px] w-full">
       <Image
-        src={LandingPage}
+        src={landingPage}
         alt="Landing Image"
         layout="fill"
         objectFit="cover"
       />
       <div className="absolute flex h-full w-full flex-col items-center justify-center">
-        <div className="flex h-1/3 w-1/3 flex-col items-center gap-10 text-center">
-          <div className="text-6xl font-bold leading-tight text-white">
-            UCR CSE’s Undergraduate Learning Assistants
-          </div>
-          <div className="w-4/5 border-[3px] border-ula-yellow-primary"></div>
+        <div className="absolute flex h-1/3 w-1/3 items-center justify-center border-b-4 border-ula-yellow-primary text-center text-6xl font-bold text-white">
+          UCR CSE’s Undergraduate Learning Assistants
         </div>
       </div>
     </div>
