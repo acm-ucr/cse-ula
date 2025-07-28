b6a8004f6d27a1bcd7e54948c07558b55d23063a
Victoria Zambrano <victoriaz20033@gmail.com>
2025-07-27 20:40:22 -0500

    Complete format issues

--- src/components/about/OurGoalsCard.tsx
+++ src/components/about/OurGoalsCard.tsx
@@ -8,7 +8,7 @@ type ourGoalsProps = {
 
 const OurGoalsCard = ({ image, text }: ourGoalsProps) => {
   return (
-    <div className="mx-8 flex w-1/4 flex-col items-center justify-center gap-y-4 border-y-4 border-ula-yellow-primary py-8 text-center text-xl font-medium">
+    <div className="mx-8 my-8 flex min-h-[283px] w-1/4 min-w-[245px] flex-col items-center justify-center gap-y-4 border-y-4 border-ula-yellow-primary text-center text-xl font-medium md:py-8">
       <Image src={image} alt="logo" />
       <div className="px-8">{text}</div>
     </div>
