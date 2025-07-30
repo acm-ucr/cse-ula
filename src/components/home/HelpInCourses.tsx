f55fdd5553294b645a68ed9dc99df810d2d26930
LivayaaSharma <livayaasharma777@gmail.com>
2025-07-31 02:18:45 +0530

    made requested changed to Help In Courses component

--- src/components/home/HelpInCourses.tsx
+++ src/components/home/HelpInCourses.tsx
@@ -1,11 +1,10 @@
+import Header from "@/components/Header";
 const HelpInCourses = () => {
   return (
-    <div className="mx-auto w-full max-w-screen-xl bg-white px-8 py-8">
-      <h2 className="mb-8 text-center text-3xl font-bold text-ula-blue-primary md:text-4xl">
-        Do you need help in your CSE courses?
-      </h2>
-      <div className="flex justify-center">
-        <div className="grid grid-cols-2 gap-8 text-base font-semibold text-black md:text-lg">
+    <div className="mx-auto w-full p-8">
+      <Header text="Do you need help in your CSE courses?" />
+      <div className="mt-5 flex justify-center">
+        <div className="grid grid-cols-1 gap-2 font-semibold md:grid-cols-2 md:gap-14 md:text-lg">
           <ul className="space-y-2">
             <li>✓ Python: CS 9A, 9B, 9C</li>
             <li>✓ Software: CS 10A, 10B, 10C</li>
