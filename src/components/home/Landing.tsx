f46e573a5b128e109516a4f64c1688c9f27e92df
Kevin Loritsch <kloritsch58@gmail.com>
2025-07-30 11:50:29 -0700

    Tweaks

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -3,15 +3,15 @@ import landingPage from "@/public/home/WCHLanding.webp";
 
 const Landing = () => {
   return (
-    <div className="relative h-[800px] w-full">
+    <div className="relative h-screen w-full">
       <Image
         src={landingPage}
         alt="Landing Image"
         layout="fill"
         objectFit="cover"
       />
-      <div className="absolute flex h-full w-full flex-col items-center justify-center">
-        <div className="absolute flex h-1/3 w-1/3 items-center justify-center border-b-4 border-ula-yellow-primary text-center text-6xl font-bold text-white">
+      <div className="absolute flex h-full w-full items-center justify-center">
+        <div className="flex h-1/3 w-1/3 place-items-center justify-center border-b-4 border-ula-yellow-primary text-center text-6xl font-bold text-white">
           UCR CSE’s Undergraduate Learning Assistants
         </div>
       </div>
