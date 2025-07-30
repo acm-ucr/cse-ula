feae4d0bd958862911e407ff3bb22c6cfc1f8242
Kevin Loritsch <kloritsch58@gmail.com>
2025-07-30 11:50:54 -0700

    '

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -12,7 +12,7 @@ const Landing = () => {
       />
       <div className="absolute flex h-full w-full items-center justify-center">
         <div className="flex h-1/3 w-1/3 place-items-center justify-center border-b-4 border-ula-yellow-primary text-center text-6xl font-bold text-white">
-          UCR CSE’s Undergraduate Learning Assistants
+          UCR CSE's Undergraduate Learning Assistants
         </div>
       </div>
     </div>
