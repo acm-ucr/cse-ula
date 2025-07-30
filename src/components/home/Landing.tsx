fc00020aefa3cd305f80e2e505b47db23fefeda2
pratheek28 <pratheek0928@gmail.com>
2025-07-30 14:42:14 -0700

    Added mobile responsiveness for landing page

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -11,7 +11,7 @@ const Landing = () => {
         objectFit="cover"
       />
       <div className="absolute flex h-full w-full items-center justify-center">
-        <div className="flex h-1/3 w-1/3 place-items-center justify-center border-b-4 border-ula-yellow-primary text-center text-6xl font-bold text-white">
+        <div className="flex h-1/3 w-1/3 place-items-center justify-center border-b-4 border-ula-yellow-primary text-center text-4xl font-bold text-white xl:text-6xl">
           UCR CSE's Undergraduate Learning Assistants
         </div>
       </div>
