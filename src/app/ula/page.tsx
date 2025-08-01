5958f05f0851c0590a7a5eebe172f1b84f0cbb63
Willow <isaiasnoel@gmail.com>
2025-08-01 13:22:16 -0700

    formatting + mapping complete

--- src/app/ula/page.tsx
+++ src/app/ula/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 const ULA = () => {
   return (
     <div>
-      <Header text="Meet the 2025-2026 ULAs!"/>
+      <Header text="Meet the 2025-2026 ULAs!" />
       <ULAs />
     </div>
   );
