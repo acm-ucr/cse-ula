cce4e2a2b21c23731e07fc97870123b6793035f6
LivayaaSharma <livayaasharma777@gmail.com>
2025-08-03 19:20:30 +0530

    animated 'Our Goals' component

--- src/components/about/OurGoals.tsx
+++ src/components/about/OurGoals.tsx
@@ -3,7 +3,7 @@
 import OurGoalsCard from "@/components/about/OurGoalsCard";
 import Header from "@/components/Header";
 import { ourGoalsCard } from "@/data/OurGoalsCards";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 
 const OurGoals = () => {
   return (
