d12233485709918fcb84a50002b34234a834e5c1
Victoria Zambrano <victoriaz20033@gmail.com>
2025-08-03 09:32:36 -0500

    Complete & Mobilize Questions Section

--- src/components/home/Questions.tsx
+++ src/components/home/Questions.tsx
@@ -1,12 +1,19 @@
 import InfoButtonHeader from "@/components/home/InfoButtonHeader";
 import blueBook from "@/public/home/blueBook.svg";
 import InfoButton from "@/components/home/InfoButton";
+import baselinePeople from "@/public/home/baselinePeople.svg";
 
 const Questions = () => {
   return (
-    <div className="">
-      <InfoButtonHeader text="What courses can I get help with?" />
-      <InfoButton icon={blueBook} text="Browse Courses" link="/about" />
+    <div className="flex flex-col justify-evenly px-2 pt-8 md:flex-row">
+      <div className="min-w-[392px] md:w-2/5">
+        <InfoButtonHeader text="What courses can I get help with?" />
+        <InfoButton icon={blueBook} text="Browse Courses" link="/about" />
+      </div>
+      <div className="min-w-[392px] md:w-2/5">
+        <InfoButtonHeader text="Who are the ULAs?" />
+        <InfoButton icon={baselinePeople} text="Meet the ULAs" link="/ula" />
+      </div>
     </div>
   );
 };
