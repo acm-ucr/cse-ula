aa0a5712142f114327a672d48308726c7c06526e
Simon <simoncun2586@gmail.com>
2025-08-03 20:00:55 -0700

    changes

--- src/components/home/Location.tsx
+++ src/components/home/Location.tsx
@@ -12,7 +12,7 @@ const textLines = [
 const Location = () => {
   return (
     <div className="flex w-full flex-col items-center justify-evenly bg-ula-blue-primary md:flex-row">
-      <div className="flex flex-col items-center gap-8 p-8 md:w-2/5">
+      <div className="flex flex-col items-center gap-8 px-8 pb-4 pt-8 md:w-2/5 md:pb-8">
         <div className="flex flex-col items-center justify-center text-left tracking-wide text-white md:hidden md:text-2xl">
           {textLines.map((line, i) => (
             <div key={i} className="mb-3">
@@ -27,11 +27,14 @@ const Location = () => {
           <Button text="Get help here!" link="/calendar" />
         </div>
       </div>
-      <div className="flex flex-col items-center px-8 pb-8 md:w-2/5 md:p-8">
+      <div className="flex flex-col items-center gap-2 px-5 pb-8 md:w-2/5 md:p-8">
         <Image src={ulaMap} alt={"ULA Map"} />
         <p className="flex justify-center text-center text-xl font-medium text-white md:text-2xl">
           Find us at Room 129 on WCH Floor 1!
         </p>
+        <div className="mt-2 md:hidden">
+          <Button text="Get help here!" link="/calendar" />
+        </div>
       </div>
     </div>
   );
