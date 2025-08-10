5eae662f47920e150618e4ad04403eaa4124adf7
Victoria Zambrano <victoriaz20033@gmail.com>
2025-08-10 12:42:45 -0500

    Fixing width + spacing

--- src/components/home/InfoButton.tsx
+++ src/components/home/InfoButton.tsx
@@ -12,13 +12,13 @@ interface infoButtonProps {
 const InfoButton = ({ icon, text, link }: infoButtonProps) => {
   return (
     <Link href={link}>
-      <div className="m-4 flex min-w-[380px] gap-8 px-6 py-5 shadow-lg">
+      <div className="m-4 ml-auto flex justify-evenly px-4 py-5 shadow-lg">
         <div className="flex gap-5">
           <Image src={icon} alt="icon" className="h-8 w-8" />
           <p className="whitespace-nowrap text-lg">{text}</p>
         </div>
 
-        <Image src={arrow} alt="arrow" className="ml-auto" />
+        <Image src={arrow} alt="arrow" className="ml-auto w-16 lg:w-20" />
       </div>
     </Link>
   );
--- src/components/home/Questions.tsx
+++ src/components/home/Questions.tsx
@@ -6,11 +6,11 @@ import baselinePeople from "@/public/home/baselinePeople.svg";
 const Questions = () => {
   return (
     <div className="flex flex-col justify-evenly px-2 pt-8 md:flex-row">
-      <div className="min-w-[392px] md:w-2/5">
+      <div className="md:w-2/5">
         <InfoButtonHeader text="What courses can I get help with?" />
         <InfoButton icon={blueBook} text="Browse Courses" link="/about" />
       </div>
-      <div className="min-w-[392px] md:w-2/5">
+      <div className="md:w-2/5">
         <InfoButtonHeader text="Who are the ULAs?" />
         <InfoButton icon={baselinePeople} text="Meet the ULAs" link="/ula" />
       </div>
