f3d033fbe17d0e73523fa9b82821bf0a48cabf1c
Kevin Loritsch <kloritsch58@gmail.com>
2025-08-12 18:53:29 -0700

    tweaks

--- src/components/about/OurGoals.tsx
+++ src/components/about/OurGoals.tsx
@@ -16,10 +16,28 @@ const OurGoals = () => {
         <Header text="Our Goals" />
       </motion.div>
 
-      <div className="flex w-full flex-wrap justify-evenly md:pt-6 lg:flex-nowrap">
+      {/* <div className="flex w-full flex-wrap justify-evenly md:pt-6 lg:flex-nowrap">
         {ourGoalsCard.map(({ image, text }, index) => (
           <motion.div
             className="flex w-[45%] justify-center"
+            key={index}
+            initial={{ opacity: 0, y: 40, scale: 0.9 }}
+            whileInView={{ opacity: 1, y: 0, scale: 1 }}
+            transition={{
+              duration: 0.6,
+              ease: "easeOut",
+              delay: index * 0.2,
+            }}
+          >
+            <OurGoalsCard image={image} text={text} />
+          </motion.div>
+        ))}
+      </div> */}
+
+      <div className="flex w-full flex-wrap justify-evenly md:flex-nowrap md:pt-6">
+        {ourGoalsCard.map(({ image, text }, index) => (
+          <motion.div
+            className="flex justify-center"
             key={index}
             initial={{ opacity: 0, y: 40, scale: 0.9 }}
             whileInView={{ opacity: 1, y: 0, scale: 1 }}
