c16d30a1964cf39c78396e15616500958f2c3e3c
lydia son <lymifrog@gmail.com>
2025-08-13 13:11:55 -0700

    Landing Page Animation

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -1,4 +1,31 @@
+"use client";
+import { motion } from "motion/react";
+import Image from "next/image";
+
 const Landing = () => {
-  return <div className="">Landing</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1, ease: "easeOut", type: "tween" }}
+      className="relative h-screen w-full overflow-hidden"
+    >
+      <Image
+        src="/home/WCHLanding.webp"
+        alt="Landing Background"
+        fill
+        style={{ objectFit: "cover" }}
+        priority
+      />
+
+      <div className="relative z-10 mx-auto flex h-full max-w-lg flex-col items-center justify-center px-4 text-center">
+        <div className="text-5xl font-bold leading-snug tracking-wide text-white">
+          UCR CSE's Undergraduate Learning Assistants
+        </div>
+        <div className="mt-6 h-1 w-3/4 bg-ula-yellow-primary"></div>
+      </div>
+    </motion.div>
+  );
 };
 export default Landing;
