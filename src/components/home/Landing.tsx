6605d13eb8feb6704274fc9853954c218d70d959
lydia son <lymifrog@gmail.com>
2025-08-17 19:39:33 -0700

    Landing Page Animation Revisions

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -5,26 +5,24 @@ import { motion } from "motion/react";
 
 const Landing = () => {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 1, ease: "easeOut", type: "tween" }}
-    >
-      <div className="relative h-screen w-full">
-        <Image
-          src={landingPage}
-          alt="Landing Image"
-          layout="fill"
-          objectFit="cover"
-        />
-        <div className="absolute flex h-full w-full items-center justify-center">
-          <div className="flex h-1/3 w-1/3 place-items-center justify-center border-b-4 border-ula-yellow-primary text-center text-4xl font-bold text-white xl:text-6xl">
-            UCR CSE's Undergraduate Learning Assistants
-          </div>
+    <div className="relative h-screen w-full">
+      <Image
+        src={landingPage}
+        alt="Landing Image"
+        layout="fill"
+        objectFit="cover"
+      />
+      <motion.div
+        className="absolute flex h-full w-full items-center justify-center"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, ease: "easeOut", type: "tween" }}
+      >
+        <div className="flex h-1/3 w-1/3 place-items-center justify-center border-b-4 border-ula-yellow-primary text-center text-4xl font-bold text-white xl:text-6xl">
+          UCR CSE's Undergraduate Learning Assistants
         </div>
-      </div>
-    </motion.div>
+      </motion.div>
+    </div>
   );
 };
 export default Landing;
