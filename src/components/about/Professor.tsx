6f177c110f9f869065c7a278850907545ec43eff
gutlapallin <nehagutlapalli@gmail.com>
2025-08-18 19:34:51 -0700

    Fixed issues

--- src/components/about/Professor.tsx
+++ src/components/about/Professor.tsx
@@ -1,43 +1,43 @@
 "use client";
 import Image from "next/image";
 import professorImage from "@/public/about/watkinsonMedina.webp";
-
 import { motion } from "motion/react";
+
+const FadeIn = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      viewport={{ once: true }}
+    >
+      {children}
+    </motion.div>
+  );
+}
 
 const Professor = () => {
   return (
     <div className="mx-8 mb-4 flex flex-row justify-center gap-8 xl:flex-row xl:items-center">
-      <motion.div
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ duration: 2.5 }}
-        viewport={{ once: true }}
-      >
+      <FadeIn>
         <Image
           src={professorImage}
           alt="Professor Watkinson Medina"
           className="font-sm mb-4 w-full rounded-md xl:w-3/4"
         />
-      </motion.div>
+      </FadeIn>
 
-      <motion.div
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ duration: 2.5 }}
-        viewport={{ once: true }}
-      >
-        <div className="w-full xl:w-4/5">
-          <p className="font-xl text-left text-lg text-black xl:text-2xl">
-            "The ULA program is a great chance to make a real difference.
-            Students get more personalized learning that helps with their
-            classes, and ULAs get to grow as educators. All in all, the ULA
-            program is a good thing for everyone who's part of it."
-          </p>
-          <p className="font-xs mt-2 text-right text-black xl:text-xl">
-            - Professor Watkinson Medina
-          </p>
-        </div>
-      </motion.div>
+      <FadeIn>
+        <p className="font-xl text-left text-lg text-black xl:text-2xl">
+          "The ULA program is a great chance to make a real difference.
+          Students get more personalized learning that helps with their
+          classes, and ULAs get to grow as educators. All in all, the ULA
+          program is a good thing for everyone who's part of it."
+        </p>
+        <p className="font-xs mt-2 text-right text-black xl:text-xl">
+          - Professor Watkinson Medina
+        </p>
+      </FadeIn>
     </div>
   );
 };
