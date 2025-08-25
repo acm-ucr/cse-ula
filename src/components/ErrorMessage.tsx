aab5e38f71cfa315803f72593a9ddd2b28c103b4
lydia son <lymifrog@gmail.com>
2025-08-24 20:12:34 -0700

    404 Animation Revision

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -10,6 +10,25 @@ type Props = {
   details: string;
 };
 
+type SampleAnimationProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+const SampleAnimation = ({ children, className }: SampleAnimationProps) => {
+  return (
+    <motion.div
+      className={className}
+      initial={{ opacity: 0, y: -20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.75, ease: "easeOut" }}
+      viewport={{ once: true, amount: 0.5 }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
 const ErrorMessage = ({ message, details }: Props) => {
   return (
     <div className="relative h-screen w-full">
@@ -33,30 +52,16 @@ const ErrorMessage = ({ message, details }: Props) => {
       </div>
 
       <div className="absolute flex h-full w-full flex-col items-center justify-center gap-4">
-        <motion.div
-          className="flex h-1/5 w-1/2 flex-col place-items-center justify-center gap-4 text-center text-white sm:w-1/5"
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, ease: "easeOut" }}
-        >
-          <motion.div
-            className="border-b-4 border-ula-yellow-primary pb-3 text-5xl font-bold xl:text-6xl"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, ease: "easeOut" }}
-          >
+        <SampleAnimation className="flex h-1/5 w-1/2 flex-col place-items-center justify-center gap-4 text-center text-white sm:w-1/5">
+          <div className="border-b-4 border-ula-yellow-primary pb-3 text-5xl font-bold xl:text-6xl">
             {message}
-          </motion.div>
+          </div>
           <div className="text-xl xl:text-2xl">{details}</div>
-        </motion.div>
+        </SampleAnimation>
 
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, ease: "easeOut" }}
-        >
+        <SampleAnimation>
           <Button text="Return home" link="./" />
-        </motion.div>
+        </SampleAnimation>
       </div>
     </div>
   );
