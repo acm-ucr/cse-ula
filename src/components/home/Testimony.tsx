1204ff590d44614202d20911fcbc66826c6d757a
Simon <simoncun2586@gmail.com>
2025-08-26 15:52:21 -0700

    import fix

--- src/components/home/Testimony.tsx
+++ src/components/home/Testimony.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import Header from "@/components/Header";
 import Card from "@/components/home/Card";
 import { testimonySections } from "@/data/Testimonies";
