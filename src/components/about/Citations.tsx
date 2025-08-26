95c4de1ec15f1f057060336ecd7fb079dbff4e60
Kevin Loritsch <kloritsch58@gmail.com>
2025-08-26 10:52:12 -0700

    update citations animation too

--- src/components/about/Citations.tsx
+++ src/components/about/Citations.tsx
@@ -24,11 +24,15 @@ const Citations = () => {
         transition={{ duration: 0.6, ease: "easeOut" }}
         className="mt-6 flex justify-center px-4 text-black"
       >
-        <div className="mb-6 w-1/2 text-center text-xl font-bold">
+        <div className="mb-6 w-2/3 text-center text-xl font-bold md:w-1/2">
           Check out the research about the ULA program!
           <motion.p
             className="my-8 text-base font-normal tracking-wide"
             {...hoverEffect}
+            transition={{
+              duration: 0.65,
+              ease: "easeInOut",
+            }}
           >
             <Link
               href="https://dl.acm.org/doi/abs/10.1145/3626253.3635630"
